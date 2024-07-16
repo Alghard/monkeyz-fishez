5d80f11aee7c575d0f5ae8769b2ecf7bbd5ad257
Victor <coolstorybrotherz@gmail.com>
2024-07-16 11:51:30 +0200

    test

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,28 @@
 import { Inter } from "next/font/google";
-import React, { useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+    const [message, setMessage] = useState("Loading...");
+    const [monkey, setMonkey] = useState([]);
 
-  const [message, setMessage] = useState("Loading...")
-  const [monkey, setMonkey] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3001/api/home")
+            .then((response) => response.json())
+            .then((data) => {
+                setMessage(data.message);
+                setMonkey(data.monkey);
+            });
+    }, []);
 
-  useEffect(() => {
-    fetch("http://localhost:3001/api/home").then(
-      response => response.json()
-    ).then(
-      data => {
-        setMessage(data.message)
-        setMonkey(data.monkey)
-      }
-    )
-  }, [])
-
-  return (
-    <main>
-      <div>{message}</div>
-      <div>{monkey.map((monkey, index) => (
-        <div key={index}>{monkey}</div>
-      ))}</div>
-    </main>
-  );
+    return (
+        <main>
+            <div>{message}</div>
+            <div>
+                {monkey.map((monkey, index) => (
+                    <div key={index}>{monkey}</div>
+                ))}
+            </div>
+            <h1>TUTUTUTUTUTU</h1>;
+        </main>
+    );
 }
