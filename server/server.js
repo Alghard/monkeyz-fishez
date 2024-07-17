924dbefa002309e90f7c72771a8f1f973ba4d236
Alghard <julien.varela@gmail.com>
2024-07-17 16:32:58 +0200

    Optimisation logique serveur

--- server/server.js
+++ server/server.js
@@ -1,32 +1,52 @@
-const express = require ('express');
-const mongoose = require("mongoose");
-const cors = require('cors');
-const app= express();
-const username = "julienvarela"; //!! A déplacer
-const password = "vZLBQiX1dF2Ye7Ed"; //!! A déplacer
-const db = mongoose.connection;
-const Monkey = require("./models/monkey");
+const http = require('http');
+const app = require('./app');
+require('dotenv/config');
 
-const PORT = 3001;
+//**la fonction normalizePort renvoie un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne ;
+const normalizePort = val => {
+  const port = parseInt(val, 10);
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${username}:${password}@cluster0.xiy2i40.mongodb.net/monkeys_fishes?retryWrites=true&w=majority&appName=Cluster0`;
-const mongo = {
-  uri: `${uri}`,
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3001');
+app.set('port', port);
+
+//**la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur ;
+const errorHandler = error => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges.');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use.');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 };
 
-mongoose.connect(mongo.uri)
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const server = http.createServer(app);
 
+server.on('error', errorHandler);
 
-app.use(cors());
-
-app.get("/api/home", (req, res, next) => {
-  res.json({ message: "Hello world !", monkey: ["sadMonke", "happyMonke", "blaiseMonke"]});
+//**un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console. */
+server.on('listening', () => {
+  const address = server.address();
+  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+  console.log('Listening on ' + bind);
 });
 
-
-app.listen(PORT, () =>{
-  console.log(`Server started on port ${PORT}`);
-})
+server.listen(port);
