67ba37c65358387966b183b3d7c39d8e92c36f5d
Alghard <148850701+Alghard@users.noreply.github.com>
2024-07-24 10:24:41 +0200

    Update monkey.js

--- server/controllers/monkey.js
+++ server/controllers/monkey.js
@@ -1,16 +1,36 @@
+//Bienvenu dans le contrôler, c'est ici qu'on va définir les fonctions qu'on a besoin, dans notre cas le CRUD( créateur, read, update, delete)
+
 const Monkey = require('../models/Monkey');
 
+
+//fonction pour ajouter un monkey en bdd
+//on a toujours besoin des paramètres req res et next(pas sur du next à verifier) req pour requête c'est à dire la demande côté front/client et res pour la réponse du serveur 
+
 exports.addMonkey = (req, res, next) => {
+// delete body ici va servir à nettoyer les données reçu par le serveur, en gros si il y avait déjà une id de monkey enregistré dans le body on va la supprimer pour pas créer de pb
   delete req.body._id;
+
+// on créer un nouvel objet Monkey basé sur son modèle en récupèrant toutes les infos transmises dans le body du serveur c'est à dire le formulaire d'ajout 
   const monkey = new Monkey({
     ...req.body
   });
+
+// save va enregistrer le monkey en bdd 
   monkey.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
-    .catch(error => res.status(400).json({ error }));
+
+    .then(() =>
+//on envoi un message de validation si tout s'est bien passé 
+ res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error =>
+//sinon on envoi un message d'erreur 
+ res.status(400).json({ error }));
 };
 
+
+// ici la seul chose qui va changer c'est qu'on va avoir besoin de l'id du monkey pour modifier supprimer ou afficher
 exports.modifyMonkey = (req, res, next) => {
+
+//c'est dans les paramètres de cette fonction qu'on va demander l'id qui est qq part dans le body
   Monkey.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Monkey mis à jour' }))
     .catch(error => res.status(400).json({ error }))
