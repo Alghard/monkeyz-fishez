87b51fd66e2ba58a26ef08ba228b97b520ba6b21
Alghard <julien.varela@gmail.com>
2024-07-30 10:08:50 +0200

    signup and login form added, server routes/controller/model for user created

---
+++ server/controllers/user.js
@@ -0,0 +1,48 @@
+//bcrypt permet de hash un mdp
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+
+const User = require('../models/User');
+
+const msg = "Paire identifiant/mot de passe incorrecte";
+
+exports.signup = async ( req, res ) => {
+
+  const { username, email, password } = req.body;
+
+  try {
+    //Je passe la fonction méthode hash de bcrypt au mot de passe contenu dans le body 
+    //le 10 indique le nombre de fois que le mdp sera hash(plus sécurisé)
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+    res.status(201).json({ message: 'Utilisateur créé avec succès' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: 'Utilisateur non trouvé' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Mot de passe incorrect' });
+    }
+    // Générer un token JWT
+    const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
+    res.status(200).json({ message: 'Connexion réussie', token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
